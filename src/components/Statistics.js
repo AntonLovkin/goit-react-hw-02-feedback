62f09beb467fee071a2ab5bdbfb8f99829538a14
AntonLovkin <anton28111987@gmail.com>
2021-06-07 23:02:34 +0300

    create Statistics.js

---
+++ src/components/Statistics.js
@@ -0,0 +1,38 @@
+import { Component } from "react";
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <div>
+    <h2>Statistics</h2>
+    <p>
+      Good:<span>{good}</span>
+    </p>
+    <p>
+      Neutral:<span>{neutral}</span>
+    </p>
+    <p>
+      Bad:<span>{bad}</span>
+    </p>
+    <p>
+      Total:<span>{total}</span>
+    </p>
+    <p>
+      Positive feedback:<span>{positivePercentage}%</span>
+    </p>
+  </div>
+);
+
+// Statistics.defaultProps = {
+//     name: "User Name",
+// };
+
+// Statistics.propTypes = {
+//     name: PropTypes.string,
+//   tag: PropTypes.string.isRequired,
+//   location: PropTypes.string.isRequired,
+//   avatar: PropTypes.string.isRequired,
+//   followers: PropTypes.number.isRequired,
+//   views: PropTypes.number.isRequired,
+//   likes: PropTypes.number.isRequired,
+// }
+
+export default Statistics;
